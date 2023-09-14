e850188ec2758e03d7c157f5492aaa14a3670f35
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-09-14 10:25:52 +0800

    Add db.json, update code accordingly
    - move persons info to db.json
    -install axios and json packages
    - update persons state
    - add function to call for persons from json file

--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -7,14 +7,12 @@ const App = () => {
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
-  useEffect(() => {
-    console.log('effect')
-    axios.get('http://localhost:3001/notes')
-    .then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    })
-  }, [])
+  
+  
+  useEffect(() =>{console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data)})}, [])
 
   console.log('render', notes.length, 'notes')
 
