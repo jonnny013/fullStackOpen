aae8607a5363476a80c99c2f8730606e3db20825
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-04 14:12:38 +0800

    Update practice

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log("Method: ", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -69,6 +83,8 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint);
 
 const PORT = 3001
 app.listen(PORT) 
