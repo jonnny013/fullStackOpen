a69850e43d86f23a9290dda02e41e71adf56fc20
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-06 13:45:23 +0800

    Update server proxy

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
-
-app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
@@ -18,6 +15,8 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(requestLogger);
 
@@ -80,6 +79,7 @@ app.post('/api/notes', (request, response) => {
   const note = {
     content: body.content,
     important: body.important || false,
+    date: new Date(),
     id: generateId(),
   }
 
