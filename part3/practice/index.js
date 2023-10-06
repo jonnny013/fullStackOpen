b17f728b1203eefed9c0c394280ae15c11b498d7
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-06 13:08:54 +0800

    Build backend and transfer front end to back

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
+
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
@@ -86,6 +90,6 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT) 
 console.log(`Server running on port ${PORT}` )
