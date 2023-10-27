b299f74935c23386fd31a0864efbcfcc0e954b12
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-27 11:45:55 +0800

    add frontend tests

--- part2/practice/src/Components/NoteForm.jsx
+++ part2/practice/src/Components/NoteForm.jsx
@@ -12,13 +12,15 @@ const NoteForm = ({ createNote }) => {
         setNewNote('')
     }
     return (
-        <div>
+        <div className='formDiv'>
             <h2>Create a new note</h2>
 
             <form onSubmit={addNote}>
                 <input
                     value={newNote}
                     onChange={event => setNewNote(event.target.value)}
+                    placeholder='write note content here'
+                    id='note-input'
                 />
                 <button type="submit">save</button>
             </form>
